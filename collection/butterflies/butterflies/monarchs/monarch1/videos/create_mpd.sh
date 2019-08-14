7820870244f61c342ec59bdeac3e6ce709b52a99
Leigh Dragoon <ladragoo@campus-012-097.ucdavis.edu>
2019-08-14 12:23:19 -0700

    Added file containing my ffmpeg test commands

--- collection/butterflies/butterflies/monarchs/monarch1/videos/create_mpd.sh
+++ collection/butterflies/butterflies/monarchs/monarch1/videos/create_mpd.sh
@@ -1,1 +1,10 @@
-ffmpeg -i mb_v_mp4/monarch_butterfly.mp4 mpd/monarch_butterfly.mpd
+# http://keycorner.org/pub/text/doc/ffmpeg-tutorial.htm
+# https://www.ostechnix.com/20-ffmpeg-commands-beginners/
+# https://ffmpeg.org/ffmpeg-formats.html
+
+#! /bin/bash
+
+# ffmpeg -i mb_v_mp4/yose-ynn24-monarchsandmilkweed_1280x720.mp4 -f hls -hls_list_size 1000000 -hls_time 60 mpd/monarch_butterfly.mpd
+# ffmpeg -i mb_v_mp4/monarch_butterfly_audio.mp4 -f dash -min_seg_duration 60 -init_seg_name 'mb_file.m4s' mpd/monarch_butterfly.mpd
+
+ffmpeg -i mb_v_mp4/monarch_butterfly_audio.mp4 mpd/monarch_butterfly.mpd
