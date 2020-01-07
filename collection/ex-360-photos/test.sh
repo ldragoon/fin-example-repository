80ec20f1a0bbb2593ab94dcc765971a44a8cc1f5
Leigh Dragoon <ladragoo@campus-029-017.ucdavis.edu>
2020-01-07 09:22:42 -0800

    Removed all non-Jekyll-related things

--- collection/ex-360-photos/test.sh
+++
@@ -1,7 +0,0 @@
-for f in photos/tin/media/images/*.jpg; do 
-    b=$(basename $f .jpg);
-    p=${b#*-};
-    echo $f;
-    echo $b;
-    echo $p;
-done;
--- import.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./collection/example_1-pets/import.sh
-./collection/example_2-photos/import.sh
-make --directory=collection/ex_sherry_lehmann ldp
